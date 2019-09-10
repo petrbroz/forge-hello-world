eecf28a240253e37be5a903b0a4fd7ba43010f19
Petr Broz <ipetrbroz@gmail.com>
2019-09-10 10:04:19 +0200

    Added Forge utils and setup env. variables.

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express');
 
 const port = process.env.PORT || 3000;
+const { FORGE_CLIENT_ID, FORGE_CLIENT_SECRET, FORGE_BUCKET } = process.env;
+
+if (!FORGE_CLIENT_ID || !FORGE_CLIENT_SECRET || !FORGE_BUCKET) {
+    console.warn('Missing some of the environment variables.');
+    return;
+}
 
 let app = express();
 app.use(express.static('public'));
