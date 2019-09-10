d5ac9f8ffe9e87987a9d4effcf2c994d59c14001
Petr Broz <ipetrbroz@gmail.com>
2019-09-10 10:06:25 +0200

    Updated server endpoint for listing documents.

--- routes/documents.js
+++ routes/documents.js
@@ -1,14 +1,24 @@
 const express = require('express');
+const { DataManagementClient, urnify } = require('forge-server-utils');
 
+const { FORGE_CLIENT_ID, FORGE_CLIENT_SECRET, FORGE_BUCKET } = process.env;
+
+let dataManagementClient = new DataManagementClient({
+    client_id: FORGE_CLIENT_ID,
+    client_secret: FORGE_CLIENT_SECRET
+});
 let router = express.Router();
 
 // GET /api/documents
-router.get('/', function (req, res) {
-    res.json([
-        { id: 'a', name: 'Document A' },
-        { id: 'b', name: 'Document B' },
-        { id: 'c', name: 'Document C' }
-    ]);
+router.get('/', async function (req, res) {
+    try {
+        const objects = await dataManagementClient.listObjects(FORGE_BUCKET);
+        res.json(objects.map(function (obj) {
+            return { id: urnify(obj.objectId), name: obj.objectKey };
+        }));
+    } catch(err) {
+        res.status(500).send(err);
+    }
 });
 
 module.exports = router;
