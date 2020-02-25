92ac9b1abebd209e69cda97b2d329510bed0879d
Petr Broz <ipetrbroz@gmail.com>
2020-02-25 17:45:56 +0100

    Cleaned up potree extension...
    ... mainly removing the UI. Instead of UI, let's just use viewer.getExtension('PotreeExtension').loadPointCloud(...);

--- public/PotreeExtension.js
+++ public/PotreeExtension.js
@@ -1,68 +1,66 @@
 class PotreeExtension extends Autodesk.Viewing.Extension {
     constructor(viewer, options) {
         super(viewer, options);
-        this._button = null;
-        this._toolbar = null;
         this._group = null;
-        this._pointcloud = null;
+        this._pointclouds = new Map();
     }
 
     load() {
+        this._group = new THREE.Group();
+        if (!this.viewer.overlays.hasScene('potree-scene')) {
+            this.viewer.overlays.addScene('potree-scene');
+        }
+        this.viewer.overlays.addMesh(this._group, 'potree-scene');
+        this.viewer.addEventListener(Autodesk.Viewing.CAMERA_CHANGE_EVENT, this.updatePointClouds.bind(this));
         console.log('PotreeExtension loaded.');
         return true;
     }
 
     unload() {
+        this.viewer.overlays.removeScene('potree-scene');
         console.log('PotreeExtension unloaded.');
         return true;
     }
 
-    onToolbarCreated() {
-        this._button = new Autodesk.Viewing.UI.Button('potree-button');
-        this._button.onClick = () => {
-            if (!this._group) {
-                this.loadPointCloud('/potree/data/lion_takanawa/cloud.js');
+    /**
+     * Adds potree model into the scene and starts streaming its data.
+     * @param {string} name Unique name of the model.
+     * @param {string} url URL of the potree model main file (typically "cloud.js").
+     * @param {THREE.Vector3} [position] Optional position to apply to the newly loaded pointcloud.
+     * @param {THREE.Vector3} [scale] Optional scale to apply to the newly loaded pointcloud.
+     * @example
+     * const ext = viewer.getExtension('PotreeExtension');
+     * ext.loadPointCloud('lion', '/potree/data/lion_takanawa/cloud.js', new THREE.Vector3(0, 0, -25), new THREE.Vector3(5, 5, 5));
+     */
+    loadPointCloud(name, url, position, scale) {
+        if (this._pointclouds.has(name)) {
+            return;
+        }
+        Potree.loadPointCloud(url, name, (ev) => {
+            const { pointcloud } = ev;
+            const { material } = pointcloud;
+            if (position) {
+                pointcloud.position.copy(position);
             }
-        };
-        this._button.setToolTip('Show Potree Model');
-        this._toolbar = new Autodesk.Viewing.UI.ControlGroup('potree-toolbar');
-        this._toolbar.addControl(this._button);
-        this.viewer.toolbar.addControl(this._toolbar);
-    }
-
-    loadPointCloud(url, position) {
-        Potree.loadPointCloud(url, 'pointcloud', (e) => {
-            this._group = new THREE.Group();
-            this._group.isPotreeGroup = true; // for debugging
-            this._group.scale.x = this._group.scale.y = this._group.scale.z = 5.0;
-            this._group.position.z = -35.0;
-
-            this._pointcloud = e.pointcloud;
-            this._pointcloud.isPotree = true; // for debugging
-            if (position !== undefined) {
-                this._pointcloud.position.copy(position);
+            if (scale) {
+                pointcloud.scale.copy(scale);
             }
-            const material = this._pointcloud.material;
             material.size = 2;
             material.pointColorType = Potree.PointColorType.RGB; //RGB | DEPTH | HEIGHT | POINT_INDEX | LOD | CLASSIFICATION
             material.pointSizeType = Potree.PointSizeType.ADAPTIVE; //ADAPTIVE | FIXED
             material.shape = Potree.PointShape.CIRCLE; //CIRCLE | SQUARE
-
-            this._group.add(this._pointcloud);
-            if (!this.viewer.overlays.hasScene('potree-scene')) {
-                this.viewer.overlays.addScene('potree-scene');
-            }
-            this.viewer.overlays.addMesh(this._group, 'potree-scene');
-            this.viewer.addEventListener(Autodesk.Viewing.CAMERA_CHANGE_EVENT, this.updatePointCloud.bind(this));
-            this.updatePointCloud();
+            this._group.add(pointcloud);
+            this._pointclouds.set(name, pointcloud);
+            this.updatePointClouds();
         });
     }
 
-    updatePointCloud() {
-        if (this._pointcloud) {
+    updatePointClouds() {
+        const pointclouds = Array.from(this._pointclouds.values());
+        if (pointclouds) {
             const camera = this.viewer.impl.camera; //.perspectiveCamera;
             const renderer = this.viewer.impl.glrenderer();
-            Potree.updatePointClouds([this._pointcloud], camera, renderer);
+            Potree.updatePointClouds(pointclouds, camera, renderer);
         }
     }
 }
